caed74f6841c8a3d7750de690fbe4365ec4776f3
Matej Simunic <matejthetree@gmail.com>
2018-09-10 22:11:22 +0200

    initial extension scaffold

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('@loopback/dist-util').loadDist(__dirname);
---
+++ src/component.ts
@@ -0,0 +1,9 @@
+import {Component, ProviderMap} from '@loopback/core';
+
+export class Loopback-next-socket.ioComponent implements Component {
+  constructor() {}
+
+  providers?: ProviderMap = {
+  };
+
+}
